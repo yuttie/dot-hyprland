719ba54511e2ac3c6606dc1b2c3559ee539861f4
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2025-09-05 16:52:39 +0900

    fix(restart_session_lock.sh): Improve reliability

--- restart_session_lock.sh
+++ restart_session_lock.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-hyprctl --instance 0 'keyword misc:allow_session_lock_restore true'
+hyprctl --instance 0 'keyword misc:allow_session_lock_restore 1'
+killall -9 hyprlock
+sleep 3
 hyprctl --instance 0 'dispatch exec hyprlock'
-hyprctl --instance 0 'keyword misc:allow_session_lock_restore false'
+sleep 3
+hyprctl --instance 0 'keyword misc:allow_session_lock_restore 0'
