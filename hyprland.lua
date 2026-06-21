baff0e171f780d91f180e7439906169cddfa8534
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2026-06-22 00:25:36 +0900

    feat(permission): enable permissions enforcement

--- hyprland.lua
+++ hyprland.lua
@@ -138,15 +138,15 @@ hl.env("GLFW_IM_MODULE", "ibus")
 -- Please note permission changes here require a Hyprland restart and are not applied on-the-fly
 -- for security reasons
 
--- hl.config({
---   ecosystem = {
---     enforce_permissions = true,
---   },
--- })
+hl.config({
+    ecosystem = {
+        enforce_permissions = true,
+    },
+})
 
--- hl.permission("/usr/(bin|local/bin)/grim", "screencopy", "allow")
--- hl.permission("/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", "screencopy", "allow")
--- hl.permission("/usr/(bin|local/bin)/hyprpm", "plugin", "allow")
+hl.permission({ binary = "/usr/bin/grim", type = "screencopy", mode = "allow" })
+hl.permission({ binary = "/usr/bin/grimblast", type = "screencopy", mode = "allow" })
+hl.permission({ binary = "/usr/libexec/xdg-desktop-portal-hyprland", type = "screencopy", mode = "allow" })
 
 
 -----------------------
