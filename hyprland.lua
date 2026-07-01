92b4327d48f7c28a1c8ea4fa1ea7417c33a66964
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2026-07-01 10:12:47 +0900

    feat(permission): grant screencopy permission to wl-mirror

--- hyprland.lua
+++ hyprland.lua
@@ -145,6 +145,7 @@ hl.config({
 })
 
 hl.permission({ binary = "/usr/bin/hyprlock", type = "screencopy", mode = "allow" })
+hl.permission({ binary = "/usr/bin/wl-mirror", type = "screencopy", mode = "allow" })
 hl.permission({ binary = "/usr/bin/grim", type = "screencopy", mode = "allow" })
 hl.permission({ binary = "/usr/bin/grimblast", type = "screencopy", mode = "allow" })
 hl.permission({ binary = "/usr/libexec/xdg-desktop-portal-hyprland", type = "screencopy", mode = "allow" })
