4839ac3952f536b819e02c33467435debf2e88d6
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2026-07-21 11:44:18 +0900

    chore(animation): adjust animations

--- hyprland.lua
+++ hyprland.lua
@@ -262,11 +262,11 @@ hl.curve("quick",          { type = "bezier", points = { {0.15, 0   }, {0.1,  1}
 -- Default springs
 hl.curve("easy",           { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
 
-hl.animation({ leaf = "global",        enabled = true,  speed = 10,   bezier = "default" })
-hl.animation({ leaf = "border",        enabled = true,  speed = 5.39, bezier = "easeOutQuint" })
-hl.animation({ leaf = "windows",       enabled = true,  speed = 4.79, spring = "easy" })
-hl.animation({ leaf = "windowsIn",     enabled = true,  speed = 4.1,  spring = "easy",         style = "popin 87%" })
-hl.animation({ leaf = "windowsOut",    enabled = true,  speed = 1.49, bezier = "linear",       style = "popin 87%" })
+hl.animation({ leaf = "global",        enabled = true,  speed = 5,    bezier = "default" })
+hl.animation({ leaf = "border",        enabled = false })
+hl.animation({ leaf = "windowsIn",     enabled = true,  speed = 3,    bezier = "easeOutQuint" })
+hl.animation({ leaf = "windowsOut",    enabled = true,  speed = 3,    bezier = "easeOutQuint" })
+hl.animation({ leaf = "windowsMove",   enabled = true,  speed = 3,    bezier = "easeOutQuint" })
 hl.animation({ leaf = "fadeIn",        enabled = true,  speed = 1.73, bezier = "almostLinear" })
 hl.animation({ leaf = "fadeOut",       enabled = true,  speed = 1.46, bezier = "almostLinear" })
 hl.animation({ leaf = "fade",          enabled = true,  speed = 3.03, bezier = "quick" })
@@ -275,9 +275,10 @@ hl.animation({ leaf = "layersIn",      enabled = true,  speed = 4,    bezier = "
 hl.animation({ leaf = "layersOut",     enabled = true,  speed = 1.5,  bezier = "linear",       style = "fade" })
 hl.animation({ leaf = "fadeLayersIn",  enabled = true,  speed = 1.79, bezier = "almostLinear" })
 hl.animation({ leaf = "fadeLayersOut", enabled = true,  speed = 1.39, bezier = "almostLinear" })
-hl.animation({ leaf = "workspaces",    enabled = true,  speed = 1.94, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "workspacesIn",  enabled = true,  speed = 1.21, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "workspacesOut", enabled = true,  speed = 1.94, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "workspacesIn",  enabled = true,  speed = 2, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "workspacesOut", enabled = true,  speed = 2, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "specialWorkspaceIn",  enabled = true,  speed = 2,   bezier = "almostLinear", style = "slidefadevert top 5%"    })
+hl.animation({ leaf = "specialWorkspaceOut", enabled = true,  speed = 1.5, bezier = "almostLinear", style = "slidefadevert bottom 5%" })
 hl.animation({ leaf = "zoomFactor",    enabled = true,  speed = 5,    bezier = "easeOutQuint" })
 
 -- Ref https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
